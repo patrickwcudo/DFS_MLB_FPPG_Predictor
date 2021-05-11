43409b08e0eae9fea2640b92d9f18994df6f64a0
Patrick Cudo <patrickwcudo@gmail.com>
2021-05-10 20:48:50 -0400

    updated eod 5_10

--- Code/model.py
+++ Code/model.py
@@ -27,6 +27,31 @@ def modeling_reg(X_train, X_test, y_train, y_test, class_list):
                  index=['R2 Score - Train','R2 Score - Test','RMSE'],
                  columns=[str(c) for c in class_list]).T
 
+def modeling_reg2(train_df, X, y, test_df, X_test_sc, class_list):
+    #import pandas
+    import pandas as pd
+    from sklearn.metrics import mean_squared_error
+    # create baseline for train_df
+    train_df['baseline'] = y.mean()
+    # create lists for scores
+    baseline_rmse = []
+    train_score = []
+    test_score = []
+    rmse = []
+    # for each classifier fit and score
+    for classifier in class_list:
+        #fit model
+        classifier.fit(X, y)
+        # add rmse
+        baseline_rmse.append(mean_squared_error(train_df['FPPG'], train_df['baseline'], squared=False))
+        rmse.append(mean_squared_error(y_true=test_df['FPPG'], y_pred=classifier.predict(X_test_sc), squared=False))
+        #add r2
+        train_score.append(classifier.score(X, y))
+        test_score.append(classifier.score(X_test_sc, test_df['FPPG']))
+    # combine into dataframe
+    return pd.DataFrame(data=[baseline_rmse, rmse, train_score, test_score],
+                 index=['Baseline - RMSE', 'Model - RMSE', 'R2 Score - Train','R2 Score - Test'],
+                 columns=[str(c) for c in class_list]).T
 
 # In[ ]:
 def modeling_class(X_train, X_test, y_train, y_test, class_list):
